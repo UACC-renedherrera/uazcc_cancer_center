a142e82692e8c383b4ca38c48b9f1ab9b15e39c5
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-04-12 11:07:40 -0700

    add location group

--- scripts/uazcc_patient_gis_processing.R
+++ scripts/uazcc_patient_gis_processing.R
@@ -15,7 +15,9 @@ az_patients <- read_rds("data/tidy/uazcc_az_patients.rds")
 # select only columns for spatial analysis
 az_patients_spatial <- az_patients %>%
   ungroup() %>%
-  select(lng, lat)
+  select(location = id,
+         lng, 
+         lat)
 
 # create spatial simple features
 az_patients_spatial <- st_as_sf(x = az_patients_spatial,
@@ -26,10 +28,13 @@ az_patients_spatial <- st_as_sf(x = az_patients_spatial,
 az_patients_spatial %>%
   ggplot() +
   geom_sf() +
-  geom_sf(data = roads) +
+  #geom_sf(data = roads) +
   xlim(-115, -109) +
   ylim(30, 38)
 
 # save to disk 
-st_write(az_patients_spatial, dsn = "data/tidy/gis/az_patients_spatial.shp")
+st_write(az_patients_spatial, 
+         dsn = "data/tidy/gis/az_patients_spatial.shp",
+         #delete_layer = TRUE,
+         layer = "location")
 
